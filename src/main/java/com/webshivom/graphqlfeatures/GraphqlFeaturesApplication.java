c7e87da19d8f8dc2b9aadfabc7ab67a7b3169da7
WEBSHIVOM <shivompandey636@gmail.com>
2024-01-14 11:54:24 +0530

    commit

---
+++ src/main/java/com/webshivom/graphqlfeatures/GraphqlFeaturesApplication.java
@@ -0,0 +1,13 @@
+package com.webshivom.graphqlfeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlFeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webshivom/graphqlfeatures/GraphqlFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webshivom.graphqlfeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
